716eaf92ae0c19754c760ad340d3a2e3e18e0a92
chris <stanulet.1@osu.edu>
2023-04-09 02:05:24 -0400

    added real email capabilities

--- src/containers/Footer/Footer.jsx
+++ src/containers/Footer/Footer.jsx
@@ -1,9 +1,9 @@
-import React, {useState} from 'react'
+import React, {useState, useRef} from 'react';
+import emailjs from '@emailjs/browser';
 
 
-import {AppWrap, MotionWrap} from '../../wrapper'
+import {AppWrap, MotionWrap} from '../../wrapper';
 import {HiOutlineMailOpen, HiPhone} from "react-icons/hi";
-import {client} from '../../client'
 
 
 import "./Footer.scss"
@@ -17,25 +17,19 @@ const Footer = () => {
   const handleChangeInput = (e) => {
     const {name, value} = e.target;
     setFormData({...formData, [name]:value})
-  }
+  };
 
-  const handleSubmit = () =>{
+  const form = useRef();
+  
+  const sendEmail = (e) => {
     setLoading(true);
 
-    const contact ={
-      _type:'contact',
-      name: name,
-      email: email,
-      message: message,
+    e.preventDefault();
+    emailjs.sendForm('service_vdwe1sd', 'template_17k8g31', form.current, 'AmXT9McgtceEJbsHt').then((result) => {
+      setIsFormSubmitted(true);
+    });
     }
 
-    client.create(contact)
-      .then(()=>{
-        setLoading(false);
-        setIsFormSubmitted(true);
-      })
-
-  }
   return (
     <>
       <h2 className='head-text'> Contact <span>Me</span></h2>
@@ -48,10 +42,11 @@ const Footer = () => {
           <HiPhone className='app__footer-icon'/>
           <a href='tel: +1 (440) 453-1860' className='p-text'>+1 (440) 453-1860</a>
         </div>
-        
+         
       </div>
       {!isFormSubmitted ?
-        <div className='app__footer-form app__flex'>
+        <form className='app__footer-form app__flex' ref={form} onSubmit={sendEmail}>
+
           <div className='app__flex'>
             <input className='p-text' type='text' name="name" placeholder='Your Name' value={name} onChange={handleChangeInput}/>
           </div>
@@ -61,8 +56,10 @@ const Footer = () => {
           <div>
             <textarea className='p-text' placeholder='Your Message' value={message} name='message' onChange={handleChangeInput}/>
           </div>
-          <button type='button' className='p-text' onClick={handleSubmit}>{loading ? 'Sending' : 'Send Message'}</button>
-        </div>
+          <button type='button' className='p-text' onClick={sendEmail}>{loading ? 'Sending' : 'Send Message'}</button>
+
+        </form>
+        
         :
         <div>
           <h3 className='head-text'>Thank you for getting in touch!</h3>
